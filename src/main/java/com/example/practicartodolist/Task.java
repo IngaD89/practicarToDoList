733f3f8f794d0a6c4d9c06646eef15326566c3c9
IngaD89 <inga.demetrashvili.1@iliauni.edu.ge>
2021-05-15 01:08:11 +0200

    para subir a heroku

--- src/main/java/com/example/practicartodolist/Task.java
+++ src/main/java/com/example/practicartodolist/Task.java
@@ -1,4 +1,12 @@
 package com.example.practicartodolist;
 
 public class Task {
+    private final String task;
+
+    public Task(String task) {
+        this.task = task;
+    }
+    public String getTask(){
+        return task;
+    }
 }
